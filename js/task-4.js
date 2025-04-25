30a0abc1747aab897020aa8a67b464b706a9837f
Serhii <sir3720@gmail.com>
2025-04-25 17:32:24 +0300

    add js task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector(".login-form");
 
+  loginForm.addEventListener("submit", (event) => {
+    // Зупиняємо стандартну поведінку браузера
+    event.preventDefault();
+
+    const {
+      elements: { email, password },
+    } = loginForm;
+
+    // Перевіряємо, чи всі поля заповнені
+    if (email.value.trim() === "" || password.value.trim() === "") {
+      alert("All form fields must be filled in");
+      return;
+    }
+
+    // Перевіряємо, чи email має правильний формат
+    const formData = {
+      email: email.value.trim(),
+      password: password.value.trim(),
+    };
+
+    // Виводимо дані в консоль
+    console.log(formData);
+
+    // Скидаємо форму
+    loginForm.reset();
+  });
+});
